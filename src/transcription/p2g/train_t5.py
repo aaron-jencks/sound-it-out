e224d94f90e0721cef8a9a6464a695f7c3dad052
aaron-jencks <aaronjencks@aol.com>
2026-04-02 22:24:06 -0400

    working on training loop

--- src/transcription/p2g/train_t5.py
+++ src/transcription/p2g/train_t5.py
@@ -1,13 +1,21 @@
 from argparse import ArgumentParser
+import logging
 from pathlib import Path
 from typing import Dict, List
 
 from cascade_config import CascadeConfig
 from pydantic import BaseModel
+import torch
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class ModelConfig(BaseModel):
     model_name: str
+    tokenizer_name: str
     hyperparameters: Dict
     checkpoint_prefix: Path
 
@@ -32,11 +40,25 @@ def load_configs(files: List[Path], default_config: Path) -> TrainConfig:
 
 
 def parse_args() -> TrainConfig:
-    ap = ArgumentParser()
+    ap = ArgumentParser('trains a p2g model')
     ap.add_argument('configs', type=Path, nargs='*', help='config files')
     ap.add_argument('--default-config', type=Path, default=Path('config/default.json'), help='default config file')
     args = ap.parse_args()
     return load_configs(args.configs, args.default_config)
+
+
+def train(ctx: TrainConfig):
+    logger.info('setting up training pipeline')
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        ctx.model.tokenizer_name,
+        use_fast=True,
+    )
+
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        ctx.model.model_name,
+        device_map="auto"
+    )
 
 
 def main():
