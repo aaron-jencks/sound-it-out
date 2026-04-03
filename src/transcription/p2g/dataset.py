7848c962847244fd458f6fd9548ae200bf0b9228
aaron-jencks <aaronjencks@aol.com>
2026-04-03 11:18:04 -0400

    working on dataset sampling

---
+++ src/transcription/p2g/dataset.py
@@ -0,0 +1,11 @@
+from datasets import Dataset
+
+from config import TrainConfig
+
+
+def load_dataset(ctx: TrainConfig, name: str) -> Dataset:
+    pass
+
+
+def create_dataset(ctx: TrainConfig) -> Dataset:
+    pass
