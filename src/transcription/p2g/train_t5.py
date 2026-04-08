4ffe7a970e1e0b00deca890f26f4c73929fba962
aaron-jencks <aaronjencks@aol.com>
2026-04-08 08:16:07 -0400

    updated cache file location

--- src/transcription/p2g/train_t5.py
+++ src/transcription/p2g/train_t5.py
@@ -64,7 +64,7 @@ def train(ctx: TrainConfig):
         batched=True,
         remove_columns=ds['train'].column_names,
         num_proc=os.cpu_count(),
-        cache_file_name=str(ctx.dataset.hf_cache / ctx.dataset.output_dataset_name / 'tokenized_train.arrow')
+        cache_file_name=str(ctx.dataset.hf_cache / ctx.dataset.output_dataset_name / 'tokens/tokenized_train.arrow')
     )
 
     tokenized_eval = ds['test'].map(
@@ -72,7 +72,7 @@ def train(ctx: TrainConfig):
         batched=True,
         remove_columns=ds['test'].column_names,
         num_proc=os.cpu_count(),
-        cache_file_name=str(ctx.dataset.hf_cache / ctx.dataset.output_dataset_name / 'tokenized_test.arrow')
+        cache_file_name=str(ctx.dataset.hf_cache / ctx.dataset.output_dataset_name / 'tokens/tokenized_test.arrow')
     )
 
     model = AutoModelForSeq2SeqLM.from_pretrained(
@@ -127,16 +127,16 @@ def train(ctx: TrainConfig):
     training_args = Seq2SeqTrainingArguments(
         output_dir=str(ctx.model.checkpoint_prefix),
         eval_strategy="steps",
-        eval_steps=500,
+        eval_steps=0.01,
         save_strategy="steps",
-        save_steps=500,
+        save_steps=0.01,
         save_total_limit=2,
         metric_for_best_model="bleu",
         load_best_model_at_end=True,
         weight_decay=0.01,
-        logging_steps=500,
-        per_device_train_batch_size=6,
-        per_device_eval_batch_size=6,
+        logging_steps=0.01,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
         predict_with_generate=True,
         num_train_epochs=3,
         report_to="wandb",
