5d6b7bc3312e474ddb0ed3558a123bfa3ed8676c
aaron-jencks <aaronjencks@aol.com>
2026-04-10 15:26:36 -0400

    fixed evaluate to allow multiple subsets

--- src/transcription/p2g/evaluate_t5.py
+++ src/transcription/p2g/evaluate_t5.py
@@ -1,6 +1,16 @@
-from config import generate_argparse, load_configs
+from pathlib import Path
+from typing import Dict
+
+from datasets import Dataset
+
+from config import generate_argparse, load_configs, TrainConfig
 from dataset import create_dataset, load_hf_dataset
-from train_t5 import generate_trainer, setup_wandb
+from train_t5 import generate_trainer
+
+
+def evaluate_dataset(ctx: TrainConfig, ds: Dataset, checkpoint: Path) -> Dict[str, float]:
+    trainer = generate_trainer(ctx, None, ds, checkpoint)
+    return trainer.evaluate()
 
 
 def main():
@@ -10,15 +20,18 @@ def main():
     args = ap.parse_args()
     conf = load_configs(args.configs, args.default_config)
 
-    if conf.evaluation_dataset is None:
+    if conf.evaluation_datasets is None:
         _, ds = create_dataset(conf)
-    else:
-        ds = load_hf_dataset(conf.evaluation_dataset, conf.dataset.hf_cache, False)
+        print(evaluate_dataset(conf, ds, args.checkpoint))
+        return
 
-    trainer = generate_trainer(conf, None, ds, args.checkpoint)
-
-    metrics = trainer.evaluate()
-    print(metrics)
+    for ds_def in conf.evaluation_datasets:
+        ds = load_hf_dataset(ds_def, conf.dataset.hf_cache, False)
+        if ds_def.subset is None:
+            print(f"{ds_def.name}({ds_def.split})")
+        else:
+            print(f"{ds_def.name}:{ds_def.subset}({ds_def.split})")
+        print(evaluate_dataset(conf, ds, args.checkpoint))
 
 
 if __name__ == "__main__":
