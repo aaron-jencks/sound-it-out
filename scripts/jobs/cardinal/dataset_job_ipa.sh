5ae645f56779e0c8f366c537864edccca78ffa76
aaron-jencks <aaronjencks@aol.com>
2026-05-16 10:48:37 -0400

    upped job runtime to 2 days

--- scripts/jobs/cardinal/dataset_job_ipa.sh
+++ scripts/jobs/cardinal/dataset_job_ipa.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=/fs/ess/PAS2836/ipa_gpt/jobs/logs/errors/%x-%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=96
-#SBATCH --time=12:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --partition=cpu
 
